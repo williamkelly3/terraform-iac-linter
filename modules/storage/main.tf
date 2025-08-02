860cf656130d441e3aa8860b190fa4ddd0bbee72
William <william.kelly711@gmail.com>
2025-08-02 11:02:11 -0400

    Create main.tf

---
+++ modules/storage/main.tf
@@ -0,0 +1,9 @@
+resource "azurerm_storage_account" "this" {
+  name                     = var.name
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+  account_tier             = var.account_tier
+  account_replication_type = var.replication_type
+
+  tags = var.tags
+}
