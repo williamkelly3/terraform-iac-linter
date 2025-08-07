a8439c680a8e7a05610669d92e5231fcca2376f9
William <william.kelly711@gmail.com>
2025-08-07 16:39:38 -0400

    Create variables.tf

---
+++ environments/prod/variables.tf
@@ -0,0 +1,20 @@
+variable "location" {
+  description = "The Azure region to deploy to."
+  type        = string
+  default     = "eastus"
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group."
+  type        = string
+  default     = "rg-prod-resources"
+}
+
+variable "tags" {
+  description = "Tags to apply to resources."
+  type        = map(string)
+  default = {
+    environment = "prod"
+    owner       = "w.kelly"
+  }
+}
