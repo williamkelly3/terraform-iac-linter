4332153d5cd050fb7125dc8ad031a4c75d17db68
William <william.kelly711@gmail.com>
2025-09-15 13:14:37 -0400

    Update main.tf

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -1,17 +1,3 @@
-terraform {
-  required_version = ">= 1.6.0"
-
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">=3.80.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-}
 
 module "storage" {
   source              = "../../modules/storage"
