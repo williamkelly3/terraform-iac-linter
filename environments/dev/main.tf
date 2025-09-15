72b8b6e76fdc3d492129ea07185d498de83bdc6e
William <william.kelly711@gmail.com>
2025-09-15 15:53:11 -0400

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -5,7 +5,7 @@ module "storage" {
   resource_group_name = var.resource_group_name
   location            = var.location
   account_tier        = "Standard"
-  replication_type    = "LRS"
+  replication_type    = "GRS"
   tags = {
     environment = "dev"
     owner       = "william.kelly"
