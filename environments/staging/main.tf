eb64b8d453181a253f4d1d328d648a6d95da445d
William <william.kelly711@gmail.com>
2025-09-15 13:10:43 -0400

    Update main.tf

--- environments/staging/main.tf
+++ environments/staging/main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = ">= 1.6.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.80.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
 }
