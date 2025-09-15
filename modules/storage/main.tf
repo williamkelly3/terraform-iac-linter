10ef5f07373eced6c03f9e221d404a6c98519cb1
William <william.kelly711@gmail.com>
2025-09-15 13:34:19 -0400

    Update main.tf

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -8,7 +8,7 @@ resource "azurerm_storage_account" "this" {
   # ✅ Security / compliance best practices
   https_traffic_only_enabled      = true
   min_tls_version                = "TLS1_2"
-  allow_blob_public_access       = false
+  allow_nested_items_to_be_public  = false
   shared_access_key_enabled      = false
   public_network_access_enabled  = false
 
