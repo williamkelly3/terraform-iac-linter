ed38ea0159d6453bd231da143cef2b42d71391f7
William <william.kelly711@gmail.com>
2025-09-15 16:16:31 -0400

    Update main.tf

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -15,18 +15,26 @@ module "storage" {
 module "vnet" {
   source              = "../../modules/vnet"
   vnet_name           = "my-vnet"
-  address_space       = ["10.0.0.0/16"]
+  address_space       = ["10.30.0.0/16"]
   location            = var.location
   resource_group_name = var.resource_group_name
   tags                = var.tags
+
   subnets = [
     {
-      name           = "subnet1"
-      address_prefix = "10.0.1.0/24"
+      name             = "app"
+      address_prefixes = ["10.30.1.0/24"]
     },
     {
-      name           = "subnet2"
-      address_prefix = "10.0.2.0/24"
+      name             = "db"
+      address_prefixes = ["10.30.2.0/24"]
+    },
+    {
+      name             = "mgmt"
+      address_prefixes = ["10.30.3.0/24"]
     }
   ]
+
+  dns_servers              = []
+  ddos_protection_plan_id  = null
 }
