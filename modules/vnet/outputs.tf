87766286c5957d11a0293805ccf54502fd814617
William <william.kelly711@gmail.com>
2025-09-15 16:12:10 -0400

    Update outputs.tf

--- modules/vnet/outputs.tf
+++ modules/vnet/outputs.tf
@@ -1,7 +1,9 @@
 output "vnet_id" {
+  description = "The ID of the virtual network."
   value = azurerm_virtual_network.this.id
 }
 
 output "subnet_ids" {
-  value = [for s in azurerm_subnet.this : s.id]
+  description = "Map of subnet IDs by name."
+  value       = { for k, v in azurerm_subnet.this : k => v.id }
 }
