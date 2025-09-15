bc48a598577d8b2005d49d21f696fb9c43431377
William <william.kelly711@gmail.com>
2025-09-15 16:10:36 -0400

    Upgrade to vnet module

--- modules/vnet/main.tf
+++ modules/vnet/main.tf
@@ -4,12 +4,38 @@ resource "azurerm_virtual_network" "this" {
   location            = var.location
   resource_group_name = var.resource_group_name
   tags                = var.tags
+  dns_servers = var.dns_servers
+
+  # Optional DDoS Protection Plan
+  dynamic "ddos_protection_plan" {
+    for_each = var.ddos_protection_plan_id != null ? [1] : []
+    content {
+      id     = var.ddos_protection_plan_id
+      enable = true
+    }
+  }
+
+  tags = var.tags
 }
 
+# Create multiple subnets
 resource "azurerm_subnet" "this" {
-  count                = length(var.subnets)
-  name                 = var.subnets[count.index].name
+  for_each = { for s in var.subnets : s.name => s }
+
+  name                 = each.value.name
   resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.this.name
-  address_prefixes     = [var.subnets[count.index].address_prefix]
+  address_prefixes     = each.value.address_prefixes
+
+  # Optional delegation
+  dynamic "delegation" {
+    for_each = lookup(each.value, "delegations", [])
+    content {
+      name = delegation.value.name
+      service_delegation {
+        name    = delegation.value.service_name
+        actions = delegation.value.actions
+      }
+    }
+  }
 }
