9241f6cad1f77ea2917f2e2a95270f00f92313e1
William <william.kelly711@gmail.com>
2025-09-16 17:06:27 -0400

    Create variables.tf

---
+++ modules/vmss/variables.tf
@@ -0,0 +1,54 @@
+variable "name" {
+  description = "Name of the VM scale set"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "Resource group name"
+  type        = string
+}
+
+variable "subnet_id" {
+  description = "Subnet ID where the VMSS will deploy"
+  type        = string
+}
+
+variable "backend_pool_id" {
+  description = "Backend pool ID from the load balancer"
+  type        = string
+  default     = null
+}
+
+variable "vm_size" {
+  description = "VM size for the scale set"
+  type        = string
+  default     = "Standard_B2s"
+}
+
+variable "instance_count" {
+  description = "Number of VM instances"
+  type        = number
+  default     = 2
+}
+
+variable "admin_username" {
+  description = "Admin username for the VM"
+  type        = string
+  default     = "azureuser"
+}
+
+variable "ssh_public_key" {
+  description = "SSH public key for admin access"
+  type        = string
+}
+
+variable "tags" {
+  description = "Tags for the VMSS"
+  type        = map(string)
+  default     = {}
+}
