9e9e507ad27cf9b1e1b2f1ab1939362489b18b25
William <william.kelly711@gmail.com>
2025-09-16 17:06:43 -0400

    Create outputs.tf

---
+++ modules/vmss/outputs.tf
@@ -0,0 +1,9 @@
+output "vmss_id" {
+  description = "The ID of the VM scale set"
+  value       = azurerm_linux_virtual_machine_scale_set.this.id
+}
+
+output "vmss_identity" {
+  description = "System-assigned managed identity of the VMSS"
+  value       = azurerm_linux_virtual_machine_scale_set.this.identity[0].principal_id
+}
