1925b885d6bb14bb08fda193ef16663dfcb21748
William <william.kelly711@gmail.com>
2025-09-17 10:49:56 -0400

    Create main.tf

---
+++ modules/monitoring/main.tf
@@ -0,0 +1,19 @@
+resource "azurerm_log_analytics_workspace" "this" {
+  name                = "${var.name}-law"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  sku                 = var.workspace_sku
+  retention_in_days   = var.retention_in_days
+
+  tags = var.tags
+}
+
+resource "azurerm_application_insights" "this" {
+  name                = "${var.name}-appi"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  workspace_id        = azurerm_log_analytics_workspace.this.id
+  application_type    = "web"
+
+  tags = var.tags
+}
