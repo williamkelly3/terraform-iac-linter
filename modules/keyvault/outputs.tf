9e19c7768da49a0b80428475c08a9e721b37f8d0
William <william.kelly711@gmail.com>
2025-09-17 10:39:25 -0400

    Create outputs.tf

---
+++ modules/keyvault/outputs.tf
@@ -0,0 +1,19 @@
+output "vault_id" {
+  description = "ID of the Key Vault"
+  value       = azurerm_key_vault.this.id
+}
+
+output "vault_uri" {
+  description = "Vault URI"
+  value       = azurerm_key_vault.this.vault_uri
+}
+
+output "cmk_id" {
+  description = "ID of the customer-managed key (if created)"
+  value       = try(azurerm_key_vault_key.this[0].id, null)
+}
+
+output "des_id" {
+  description = "ID of the disk encryption set (if created)"
+  value       = try(azurerm_disk_encryption_set.this[0].id, null)
+}
