cc9ad681a60007602ce0183cd0f75ab633ab648a
William <william.kelly711@gmail.com>
2025-09-17 15:47:44 -0400

    Update main.tf

--- modules/vnet/main.tf
+++ modules/vnet/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_virtual_network" "this" {
-  name                = var.vnet_name
+  name                = var.name
   address_space       = var.address_space
   location            = var.location
   resource_group_name = var.resource_group_name
