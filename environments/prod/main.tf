250a27fd16c95d614f2919950493978efd898590
William <william.kelly711@gmail.com>
2025-09-17 15:49:58 -0400

    Update main.tf

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -1,6 +1,6 @@
 module "vnet" {
   source              = "../../modules/vnet"
-  vnet_name                = "prod-vnet"
+  name                = "prod-vnet"
   location            = var.location
   resource_group_name = var.resource_group_name
   address_space       = ["10.30.0.0/16"]
