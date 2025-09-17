8d7e1269068615b892852dd9ac1459921f23f69d
William <william.kelly711@gmail.com>
2025-09-17 16:19:55 -0400

    Update main.tf

--- modules/monitoring/diagnostic_settings/main.tf
+++ modules/monitoring/diagnostic_settings/main.tf
@@ -1,22 +1,18 @@
 resource "azurerm_monitor_diagnostic_setting" "this" {
-  name               = "${var.target_resource_name}-diag"
-  target_resource_id = var.target_resource_id
+  name                       = "${var.target_resource_id}-diagnostic"
+  target_resource_id         = var.target_resource_id
   log_analytics_workspace_id = var.log_analytics_workspace_id
 
-  # Enable common categories, resource-specific categories can be added per type
-  dynamic "log" {
-    for_each = var.log_categories
-    content {
-      category = log.value
-      enabled  = true
-    }
+  enabled_log {
+    category = "AuditEvent"
   }
 
-  dynamic "metric" {
-    for_each = var.metric_categories
-    content {
-      category = metric.value
-      enabled  = true
-    }
+  enabled_log {
+    category = "Administrative"
+  }
+
+  metric {
+    category = "AllMetrics"
+    enabled  = true
   }
 }
