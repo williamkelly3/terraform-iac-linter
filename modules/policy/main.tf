d52fd70271e0c9f0bc9538bf7eb432a2fac73750
William <william.kelly711@gmail.com>
2025-09-18 17:58:15 -0400

    Update main.tf

--- modules/policy/main.tf
+++ modules/policy/main.tf
@@ -1,10 +1,14 @@
-resource "azurerm_policy_assignment" "tls_policy" {
-  name                 = "enforce-tls12"
-  scope                = var.scope
-  policy_definition_id = "/providers/Microsoft.Authorization/policyDefinitions/cbcf3bd9-6a1d-4a82-8b6f-28d71e1db9e0"
+resource "azapi_resource" "tls_policy" {
+  type      = "Microsoft.Authorization/policyAssignments@2021-06-01"
+  name      = "enforce-tls12"
+  parent_id = var.scope
 
-  display_name     = "Enforce TLS 1.2 or higher"
-  description      = "Ensures that only TLS 1.2+ is allowed for Azure resources."
-  enforcement_mode = true
+  body = jsonencode({
+    properties = {
+      displayName  = "Enforce TLS 1.2 or higher"
+      description  = "Ensures that only TLS 1.2+ is allowed for Azure resources."
+      policyDefinitionId = "/providers/Microsoft.Authorization/policyDefinitions/cbcf3bd9-6a1d-4a82-8b6f-28d71e1db9e0"
+      enforcementMode    = "Default"
+    }
+  })
 }
-
