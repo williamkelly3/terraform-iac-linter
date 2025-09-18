20c7c251dd53abb049f887496c22187e50e68d6a
William <william.kelly711@gmail.com>
2025-09-18 14:36:13 -0400

    Update outputs.tf

--- modules/vmss/outputs.tf
+++ modules/vmss/outputs.tf
@@ -7,3 +7,7 @@ output "vmss_identity" {
   description = "System-assigned managed identity of the VMSS"
   value       = azurerm_linux_virtual_machine_scale_set.this.identity[0].principal_id
 }
+
+output "identity_principal_id" {
+  value = azurerm_linux_virtual_machine_scale_set.this.identity[0].principal_id
+}
