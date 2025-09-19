fd51e906f9530ddcb660250c9d07ff0db73ad58d
William <william.kelly711@gmail.com>
2025-09-18 23:31:22 -0400

    Update main.tf

--- modules/policy/main.tf
+++ modules/policy/main.tf
@@ -3,12 +3,12 @@ resource "azapi_resource" "tls_policy" {
   name      = "enforce-tls12"
   parent_id = var.scope
 
-  body = jsonencode({
+  body = {
     properties = {
-      displayName  = "Enforce TLS 1.2 or higher"
-      description  = "Ensures that only TLS 1.2+ is allowed for Azure resources."
+      displayName        = "Enforce TLS 1.2 or higher"
+      description        = "Ensures that only TLS 1.2+ is allowed for Azure resources."
       policyDefinitionId = "/providers/Microsoft.Authorization/policyDefinitions/cbcf3bd9-6a1d-4a82-8b6f-28d71e1db9e0"
       enforcementMode    = "Default"
     }
-  })
+  }
 }
